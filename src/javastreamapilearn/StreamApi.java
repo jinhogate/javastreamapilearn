5f43de415b7ab880939922ed8ffaf89af877bf5e
GBEGNON kossi <kossijorjinho.gbegnon@gmail.com>
2022-11-14 00:23:41 +0100

    Some stream operations

--- src/javastreamapilearn/StreamApi.java
+++ src/javastreamapilearn/StreamApi.java
@@ -1,8 +1,12 @@
 package javastreamapilearn;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamApi {
@@ -30,6 +34,58 @@ public class StreamApi {
 		Stream<Integer> streamParallel = list.parallelStream();
 		streamParallel.forEach(System.out::println);
 		
+		// Some operations you can use on stream
+		list.add(6);
+		list.add(7);
+		long count = list.stream().distinct().count();
+		System.out.println("Count : " + count);
+		List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+		Stream<Integer> streamList2 = list2.stream();
+		list2.add(6);
+		long count2 = streamList2.distinct().count();
+		System.out.println("Count2 : " + count2);
+		
+		// Iterating
+		System.out.println("Iterating");
+		boolean found  = list2.stream().anyMatch(i -> i<4);
+		System.out.println(found);
+		
+		// Filtering
+		System.out.println("Filtering");
+		List<String> list3 = Arrays.asList("One","OneAndOnly","Derek","Change","");
+		Stream<String> streamList3 = list3.stream().filter(s -> s.length()>4);
+		streamList3.forEach(System.out::println);
+		
+		// Mapping
+		System.out.println("Mapping");
+		List<String> uris = new ArrayList<>();
+		uris.add("C:\\My.txt");
+		Stream<Path> streamUris = uris.stream().map(uri -> Paths.get(uri));
+		streamUris.forEach(System.out::println);
+		list.stream().peek(n -> System.out.print(n)).map(n -> n*n).forEach(System.out::println);
+		
+		// Matching
+		System.out.println("Matching");
+		boolean anyMatch = list.stream().anyMatch(n-> n>4);
+		boolean allMatch = list.stream().allMatch(n -> n>4);
+		boolean noneMatch = list.stream().noneMatch(n -> n>4);
+		System.out.println(anyMatch + " " + allMatch + " " + noneMatch);
+		System.out.println(Stream.empty().allMatch(Objects::nonNull));
+		
+		// Reduction
+		System.out.println("Reduction");
+		list.stream().forEach(System.out::print);
+		System.out.println("");
+		System.out.println(list.stream().reduce(10, (a,b) -> a+b));
+		System.out.println(list.stream().min((a,b) -> a.compareTo(b)).get());
+		System.out.println(list.stream().max((a,b) -> a.compareTo(b)).get());
+		
+		// Collecting
+		System.out.println("Collecting");
+		List finalList = list.stream().distinct().filter(n -> n>4).collect(Collectors.toList());
+		finalList.stream().forEach(System.out::println);
 	}
+	
+	
 
 }
