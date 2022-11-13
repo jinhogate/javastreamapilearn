1c39bcfb7bfca15a25ae03fccf07b6594c3d447a
GBEGNON kossi <kossijorjinho.gbegnon@gmail.com>
2022-11-13 23:03:17 +0100

    Creation of stream

--- src/javastreamapilearn/StreamApi.java
+++ src/javastreamapilearn/StreamApi.java
@@ -1,8 +1,30 @@
 package javastreamapilearn;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class StreamApi {
 	
 	public static void main (String[] args) {
+		
+		// Stream Creation
+		
+			// Creation Stream from array
+		String[] arr = {"a","b","c"};
+		Stream<String> stream = Arrays.stream(arr);
+		stream.forEach(System.out::println);
+		
+			// Creation Stream with key 'of'
+		Stream<String> streamOf = Stream.of("Kossi","Koffi","Kodjo");
+		streamOf.forEach(System.out::println);
+		
+			// Creation Stream from a list
+		List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+		Stream<Integer> streamList = list.stream();
+		streamList.forEach(System.out::println);
+		
 		
 	}
 
